9fa31c1b3bb1c605318420c2f37e53084c2057d8
zidan <zidan@DESKTOP-DF9AGRJ.mshome.net>
2022-06-07 20:08:46 -0500

    Update

---
+++ src/main/java/pe/unjfsc/quiz/java11/logical/CLCliente.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pe.unjfsc.quiz.java11.logical;
+
+/**
+ *
+ * @author zidan
+ */
+public class CLCliente {
+    
+}
