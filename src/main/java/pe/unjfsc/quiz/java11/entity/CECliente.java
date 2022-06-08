fd6db3f0b0c4cd78ce9f881c754f1c5f7999a59c
zidan <zidan@DESKTOP-DF9AGRJ.mshome.net>
2022-06-07 20:14:26 -0500

    Cambiar Nombre

--- src/main/java/pe/unjfsc/quiz/java11/entity/CECliente.java
+++ src/main/java/pe/unjfsc/quiz/java11/entity/CECliente.java
@@ -5,4 +5,10 @@ public class CECliente {
     String OrdenP;
     String Orden;
     String nombre;
+    double ImpPedido;
+    //generar
+    double Impuesto;
+    double TotalProy;
+    double Retencion;
+    
 }
